4b6157678ee6981bf9bda33c0ecdb20e24eefefd
Alex Graham <ae.graham93@gmail.com>
2023-04-13 21:19:48 +1000

    first commit of working calculator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useRef} from "react"; 
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+function App() { 
+  const inputRef = useRef(null); 
+  const resultRef = useRef(null); 
+  const [result, setResult] = useState(0); 
+ 
+  function plus(e) { 
+    e.preventDefault(); 
+    setResult((result) => result + Number(inputRef.current.value)); //Number() -> creates a number from a string for example. Here we are adding the result state and the value the user enters, hence why we need to reference the value of the input field.
+  }; 
+ 
+  function minus(e) { 
+  	e.preventDefault(); 
+    setResult((result) => result - Number(inputRef.current.value)); 
+  };
+ 
+  function times(e) { 
+    e.preventDefault(); 
+    setResult((result) => result * Number(inputRef.current.value));  
+  }; 
+ 
+  function divide(e) { 
+    e.preventDefault(); 
+    setResult((result) => result / Number(inputRef.current.value));
+  };
 
-export default App;
+  function resetInput(e) { 
+    e.preventDefault(); 
+    inputRef.current.value = 0; 
+  }; 
+ 
+  function resetResult(e) { 
+  	e.preventDefault(); 
+    setResult((result) => result * 0); 
+  }; 
+ 
+  return ( 
+    <div className="App"> 
+      <div> 
+        <h1>Simplest Working Calculator</h1> 
+      </div> 
+      <form> 
+        <p ref={resultRef}>{result}</p> 
+        <input
+          pattern="[0-9]" 
+          ref={inputRef} 
+          type="number" 
+          placeholder="Type a number" 
+        /> 
+        <button onClick={plus}>&#43;</button> 
+        <button onClick={minus}>&#8722;</button>  
+        <button onClick={times}>&#215;</button>  
+        <button onClick={divide}>&#247;</button> 
+        <button onClick={resetInput}>reset input</button> 
+        <button onClick={resetResult}>reset result</button> 
+      </form> 
+    </div> 
+  ); 
+} 
+ 
+export default App; 
